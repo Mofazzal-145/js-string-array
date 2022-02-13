88823612f54677029e73f9fabafedf799ba9d3fe
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-14 01:12:13 +0600

    uppercase and lowercase

---
+++ case-converter.js
@@ -0,0 +1,9 @@
+const anthem = 'Amar sonar bangla ami tomai valobasi';
+const search = 'valo';
+
+
+const userInput = 'blACkPinK';
+const savedUserName = 'blackPinK';
+if(userInput.toLowerCase() == savedUserName.toLowerCase()){
+    console.log('user exists');
+}
