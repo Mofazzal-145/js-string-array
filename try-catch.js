45b46004809c39706227fe578bb4d45c5e2e4b26
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-14 21:24:40 +0600

    try-catch

---
+++ try-catch.js
@@ -0,0 +1,13 @@
+const myName = "ami";
+/* for(let i = 0; i < 10 i++){
+
+} */
+// name = "mofazzal";
+try{
+    myName = "mofazzal";
+}
+catch(er){
+    console.log('getting error',er);
+}
+
+console.log(myName);
